eccd3f5fe2ac43a0c5c1198d90fb3a05d67f1b91
sl4 <3940@holbertonschool.com>
2022-09-20 13:29:10 -0300

    ex 1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,6 +1,16 @@
 import { useState } from 'react'
 
+
+
 const App = () => {
+  const handl = (arr) => {
+    // copies points array
+    const copy = { ...arr }
+    //copy now reflects the new changes
+    copy[selected] += 1
+    //now we update points state with copy's value
+    setPoints(copy)
+  }
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -10,12 +20,14 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
-   
+  const [points, setPoints] = useState(new Uint8Array(7))
   const [selected, setSelected] = useState(0)
 
   return (
     <div>
       {anecdotes[selected]} <br/>
+      <p>has {points[selected]} votes</p>
+      <button onClick={() => handl(points)}>vote</button>
       <button onClick={() => setSelected(selected + 1)}>next anecdote</button>
     </div>
   )
