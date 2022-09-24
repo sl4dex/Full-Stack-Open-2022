1eb6d47b1441e85256c5aa015b23ad1dcf3fb4cb
sl4 <3940@holbertonschool.com>
2022-09-24 16:48:08 -0300

    ex 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,28 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      id: 1}
   ]) 
   const [newName, setNewName] = useState('')
 
-  const addName = (event) =>{
+  const addPerson = (event) =>{
     event.preventDefault()
+    for (let key in persons) {
+      if (persons[key].name === newName) {
+        alert(`"${newName}" is already added to phonebook`)
+        return
+      }
+    }
     const newPerson = {
-      name: newName
+      name: newName,
+      id: persons.length + 1
     }
     setPersons(persons.concat(newPerson))
     setNewName('')
   }
+
 
   const writingName = (event) => {
     setNewName(event.target.value)
@@ -22,7 +32,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={writingName}/>
         </div>
@@ -31,7 +41,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(p => <p>{p.name}</p>)}
+      {persons.map(p => <p key={p.id}>{p.name}</p>)}
     </div>
   )
 }
