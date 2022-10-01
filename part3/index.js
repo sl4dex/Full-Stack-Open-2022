7ffa0bf49a71a4a12b03b53cc67082765e61bfa8
sl4 <3940@holbertonschool.com>
2022-10-01 11:25:46 -0300

    ex 3.2, 3.3

--- part3/index.js
+++ part3/index.js
@@ -20,11 +20,28 @@ let persons = [
 ]
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+    // we use .send because its a string
+    response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    response.send(persons)
+    // we use .json beacause its json notation 
+    response.json(persons)
+})
+
+// using express two dot notation, :id will be an arbitrary string, accesed by request.params.id
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(elem => elem.id === id) 
+    if (person)
+        response.json(person)
+    else
+        response.status(404).end()
+})
+
+app.get('/api/info', (request, response) => {
+    var currentTime = new Date();
+    response.send(`<p>phonebook has info for ${persons.length} people</p> ${currentTime}`)
 })
 
 const PORT = 3001
